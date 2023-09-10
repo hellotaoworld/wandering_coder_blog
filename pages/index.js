7be327a4b8de7746bbf4b880161c401e7cf19270
t32lin <toronto.taolin@gmail.com>
2023-09-10 15:38:06 -0400

    graphql connection setup

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' }
-]
-
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <main>
       <div className='container mx-auto px-10 mb-8'>
@@ -16,11 +11,13 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-          <div className='lg:col-span-8 col-span-1'>{posts.map((post, index) => (
-            <div className='lg:col-span-8 col-span-1'>
-              <PostCard post={post} key={post.title} />
-            </div>
-          ))}</div>
+          <div className='lg:col-span-8 col-span-1'>
+            {posts.map((post, index) => (
+              <div className='lg:col-span-8 col-span-1'>
+                <PostCard post={post} key={post.title} />
+              </div>
+            ))}
+          </div>
           <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relative top-8'>
               <PostWidget />
@@ -33,4 +30,11 @@ export default function Home() {
 
     </main>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts }
+  }
 }
